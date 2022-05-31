8a14ce95dccb1a7ed48d64ad3552a697244b63d3
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-31 10:18:18 +0300

    Example getters for element

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function logMessage(){
+  alert('message')
+}
+
+// const [,button] = document.getElementsByTagName('button');
+// button.addEventListener('click', logMessage);
+
+// const id = 'unique';
+// const unique = document.getElementById(id);
+//const unique = document.getElementById('unique'); //O(1)
+// unique.addEventListener('click', logMessage);
+
+//const unique = document.querySelector('#unique');  //O(1)
+//const btn = document.querySelector('button:last-of-type');
+
+// const btns1 = document.getElementsByClassName('btn');
+// const btns2 = document.querySelectorAll('.btn');
