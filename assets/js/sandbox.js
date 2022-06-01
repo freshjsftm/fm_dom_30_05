866dd14259dc8712b4c9761730cf0b5b4d1783c5
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-01 10:47:43 +0300

    Add createAdder

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,26 +1,60 @@
-'use strict';
+"use strict";
 
 let i = 123;
 
-function createCounter(){
-  let i = 0;
+function createCounter() {
+  let i = 0; //closure
   return {
-    increment(){
+    increment() {
       return ++i;
     },
-    decrement(){
+    decrement() {
       return --i;
     },
   };
 }
 
-const counter = createCounter(); 
+const counter = createCounter();
 console.log(counter.increment());
 console.log(counter.decrement());
 
 const counter2 = createCounter();
 
+function createCounter1() {
+  let i = 0; //closure
+  return function counter() {
+    return ++i;
+  };
+}
+
+function createCounter2(initial = 0) {
+  //closure - initial
+  return function counter() {
+    return ++initial;
+  };
+}
+
+// function createAdder(n) {
+//   //let sum = n; //closure
+//   return function adder(m) {
+//     return (n += m);
+//   };
+// }
+
+// function createAdder(n){
+//   return (m)=> n+=m;
+// }
+
+const createAdder = (n=0) => (m=0) => (n += m);
 
 
+const adder = createAdder(100);
+console.log(adder(15)); // 115
+console.log(adder(50)); // 165
+console.log(adder(10)); // 175
 
-
+// const createAdder = ((n) => ((m=0) => n+=m))
+// const adder = createAdder(100)
+// adder(15)
+// adder(150)
+// console.log(adder(0))
