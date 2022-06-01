647fd3df2743c622c83ce9da37f71e300b84c1af
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-01 09:59:07 +0300

    Example return object with closure

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -2,25 +2,23 @@
 
 let i = 123;
 
-function createCounter1(){
-  //debugger
-  let i = 0; //closure
-  return function counter(){
-    return ++i;
-  }
+function createCounter(){
+  let i = 0;
+  return {
+    increment(){
+      return ++i;
+    },
+    decrement(){
+      return --i;
+    },
+  };
 }
 
-function createCounter2(initial = 0){//closure - initial
-  return function counter(){
-    return ++initial;
-  }
-}
+const counter = createCounter(); 
+console.log(counter.increment());
+console.log(counter.decrement());
 
-
-
-const counter = createCounter2(10); //let initial = 10;
-console.log(counter());
-console.log(counter());
+const counter2 = createCounter();
 
 
 
