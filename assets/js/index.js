8d78bf6940c29ac686c44d898586025234353f03
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-06 12:00:04 +0300

    Add HOMEWORK

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,19 @@
 "use strict";
-const [link] = document.links;
-link.addEventListener("click", (e) => {
-  e.preventDefault();
-});
-
-const form1 = document.getElementById("rootForm");
-const form2 = document.forms["rootForm"];
+const messages=[];
 const form = document.forms.rootForm;
-//const inpText = document.forms.rootForm.elements.inputText;
+const list = document.getElementById('list');
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(e.target.elements.inputText.defaultValue);
-  console.dir(e.target.elements.inputText.value);
+  //при нажатии на кнопку Send, добавлять значение из инпута в массив,
+  //в том случае, если оно не пустое
+  const inputValue = e.target.elements.inputText.value.trim();
+  if(inputValue){
+    messages.push(inputValue);
+    //добавить автоматическую очистку формы
+    e.target.reset();
+  }
+  //HOMEWORK
+  //рендерить в список list все сообщения из массива  
+  //console.log(messages);
 });
