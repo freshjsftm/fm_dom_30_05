a01459f88ea0d1b84095c6a29f325f360b0e80e6
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-06 11:30:01 +0300

    Add submit at form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,16 @@
-'use strict';
+"use strict";
+const [link] = document.links;
+link.addEventListener("click", (e) => {
+  e.preventDefault();
+});
 
-const form1 = document.getElementById('root-form');
-const form2 = document.forms['root-form'];
+const form1 = document.getElementById("rootForm");
+const form2 = document.forms["rootForm"];
+const form = document.forms.rootForm;
+//const inpText = document.forms.rootForm.elements.inputText;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e.target.elements.inputText.defaultValue);
+  console.dir(e.target.elements.inputText.value);
+});
