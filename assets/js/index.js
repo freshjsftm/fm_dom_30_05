bdb6d4d6ae7706fff2d2cedece470f116f429284
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-06 10:10:39 +0300

    Refactor v2.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,55 +5,73 @@ const HTMLCards = actors
                     .map((actor)=>createCard(actor));
 
 function createCard(actor){
-  const card = document.createElement('li');
-  card.classList.add('card-wrapper');
-
-  const container = document.createElement('article');
-  container.classList.add('card-container');
-
-  const fullname = document.createElement('h2');
-  fullname.classList.add('card-fullname');
-  fullname.append(document.createTextNode(actor.name || 'Noname'));
-
-  const description = document.createElement('p');
-  description.classList.add('card-description');
-  description.append(document.createTextNode(actor.birthdate || 'Month d, YYYY'));
-
-  container.append(createWrapper(actor), fullname,description);
-
-  card.append(container);
-  return card;
+  return createElement('li',{classNames:['card-wrapper']}, 
+    createElement('article',{classNames:['card-container']},
+      createWrapper(actor),
+      createElement('h2',{classNames:['card-fullname']},
+        document.createTextNode(actor.name || 'Noname')
+      ),
+      createElement('p',{classNames:['card-description']},
+        document.createTextNode(actor.birthdate || 'Month d, YYYY')
+      ),
+    )
+  );
 }
 
 cardsList.append(...HTMLCards);
+/**
+ * 
+ * @param {string} tag 
+ * @param {object} options 
+ * @param {string[]} options.classNames
+ * @param {string} options.typeEvent
+ * @param {function} options.handlerEvent
+ * @param {object[]} children 
+ * @returns 
+ */
+function createElement(tag, {classNames, typeEvent, handlerEvent}, ...children){
+  const element = document.createElement(tag);
+  element.classList.add(...classNames);
+  element.addEventListener(typeEvent, handlerEvent);
+  element.append(...children);
+  return element;
+}
 
-function createImage({photo, name}){
+function createImage({id, photo, name}){
   const img = document.createElement('img');
   img.classList.add('card-photo');
   img.setAttribute('src', photo);
   img.setAttribute('alt', name);
+  img.dataset.id = `wrapper-${id}`;
   img.addEventListener('error', photoErrorHandler);
-  return img;
+  img.addEventListener('load', photoLoadHandler);//добавили картинку родителю
 }
 
 function createWrapper(actor){
-  const {name} = actor;
+  const {id, name} = actor;
   const photoWrapper = document.createElement('div');
   photoWrapper.classList.add('card-photo-wrapper');
+  photoWrapper.setAttribute('id', `wrapper-${id}`);
 
   const initials = document.createElement('div');
   initials.classList.add('card-initials');
   initials.style.backgroundColor = stringToColour(name);
   initials.append(document.createTextNode(name[0]|| 'NN')); /* home work */
 
-  photoWrapper.append(initials, createImage(actor));
+  photoWrapper.append(initials);
+  createImage(actor);
   return photoWrapper;
 }
 
+function photoLoadHandler({target}){
+  const parent = document.getElementById(target.dataset.id);
+  parent.append(target);
+}
 
 /* handler */
 function photoErrorHandler({target}){
   target.remove();
+  return;
 }
 
 /* utilits */
