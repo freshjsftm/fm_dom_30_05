e1fd8deea4d4b4ddd577ce4bf0d05feab8c5a37e
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-07 11:35:33 +0300

    Example create RegExp

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,15 +5,13 @@ const list = document.getElementById('list');
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  //при нажатии на кнопку Send, добавлять значение из инпута в массив,
-  //в том случае, если оно не пустое
   const inputValue = e.target.elements.inputText.value.trim();
   if(inputValue){
     messages.push(inputValue);
-    //добавить автоматическую очистку формы
     e.target.reset();
   }
   //HOMEWORK
   //рендерить в список list все сообщения из массива  
   //console.log(messages);
 });
+
--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,12 +1,16 @@
 "use strict";
 
-const user={
-  info:{
-    name:'Elon',
-    contact:{
-      phone: '123'
-    }
-  }
-} 
-//const phone = user.info.contact.phone;
-const {info:{contact:{phone}}} = user;
+// const pattern1 = /\b\w{4}\b/gi;
+// const pattern2 = new RegExp('\\b\\w{4}\\b', 'gi');
+
+const str = `wqe 1999god zxc 1288USD qwerty`;
+//`wqe 1999 year zxc 12USD qwerty`
+
+const pattern1 = /\b(\d{4})[a-z]+\b/g;
+const pattern2 = /\b(\d+)([A-Z]+)\b/g;
+if(pattern2.test(str)){
+  //console.log(str.replace(pattern1, '$1 year'))
+  console.log(str.replace(pattern2, '$2 $1'))
+}else{
+  console.log(2)
+}
