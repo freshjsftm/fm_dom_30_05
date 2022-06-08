1a9272ca5ca6f89296d2a4fac06cae0c1a5995d0
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-08 09:33:28 +0300

    Refactor form & list v1.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,37 @@
 "use strict";
-const messages=[];
-const form = document.forms.rootForm;
+const state=[];
+const form = document.getElementById('rootForm');
 const list = document.getElementById('list');
-
-//const pattern = /^([A-Z][a-z]+ [A-Z]\.)|([А-Я][а-я]+ [А-Я]\.)$/;
-const pattern = /^(19[2-9][0-9])|(20([01][0-9]|2[0-2]))$/;
-//шаблон проверки валидного названия файла
+const pattern = /^[A-Z][a-z]{1,12}$/;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const inputValue = e.target.elements.inputText.value.trim();
+  const {target, target:{elements:{inputText}}} = e;
+  const inputValue = inputText.value.trim();
   if(pattern.test(inputValue)){
-    messages.push(inputValue);
-    e.target.reset();
+    state.push(inputValue);
+    target.reset();
+    const li = createElement('li',{classNames:['item']},document.createTextNode(inputValue));
+    /* Добавить в li кнопку с крестиком */
+    list.append(li);
   }
-  //HOMEWORK
-  //рендерить в список list все сообщения из массива  
-  console.log(messages);
 });
+
+/**
+ * 
+ * @param {string} tag 
+ * @param {object} options 
+ * @param {string[]} options.classNames
+ * @param {objects} children 
+ * @returns 
+ */
+ function createElement(tag, {classNames=[]}, ...children){
+  const element = document.createElement(tag);
+  if(classNames.length){
+    element.classList.add(...classNames);
+  }  
+  element.append(...children);
+  return element;
+}
 
 
