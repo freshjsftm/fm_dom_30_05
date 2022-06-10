b1a8b27168bf2f04923b83bab9d36ae2a828b1a9
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-10 11:01:54 +0300

    Example promisification

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,31 +1,20 @@
 "use strict";
 
-console.log('start');   ///sin
-setTimeout(             ///sin
-  ()=>{
-  console.log('setTimeout');    //makro
-},0);
-const pr = new Promise((resolve, reject)=>{  ///sin
-  console.log('start promise');   ///sin
-  resolve();                      ///sin
-  reject();                       ///sin
-  console.log('end promise');     ///sin
-});
-pr                                ///sin
-  .then(                          ///sin
-    ()=>{                         //mikro
-    console.log('resolve start');        ///sin
-    setTimeout(()=>{
-      console.log('setTimeout 2');    //makro
-    },0);
-    console.log('resolve end'); 
-  })
-  .catch(                          ///sin
-    ()=>{                         //mikro
-    console.log('reject')
-  })
-  .finally(                          ///sin
-    ()=>{                         //mikro
-    console.log('finally')
-  })
-console.log('end');                   ///sin
+function handlePromise(promise) {
+  return promise
+    .then((data) => {
+      console.log("in func: ", data);
+    })
+    .catch((err) => {
+      console.error("in func: ", err);
+    });
+}
+
+const number = 45;
+
+// const numberPromise = new Promise((resolve, reject)=>{
+//   resolve(number);
+// })
+
+handlePromise(Promise.resolve(number));
+handlePromise(Promise.reject('error'));
