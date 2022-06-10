d2536aa732ab212976b0f38b0f097c93da0c9316
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-10 10:09:36 +0300

    Example try/catch/finally

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,16 +1,27 @@
 "use strict";
 
-// const pattern1 = /\b\w{4}\b/gi;
-// const pattern2 = new RegExp('\\b\\w{4}\\b', 'gi');
+// try {} catch{}
+// try {} finally{}
 
-const str = `wqe 1999god zxc 1288USD qwerty`;
-//`wqe 1999 year zxc 12USD qwerty`
-
-const pattern1 = /\b(\d{4})[a-z]+\b/g;
-const pattern2 = /\b(\d+)([A-Z]+)\b/g;
-if(pattern2.test(str)){
-  //console.log(str.replace(pattern1, '$1 year'))
-  console.log(str.replace(pattern2, '$2 $1'))
-}else{
-  console.log(2)
+console.log("start");
+try {
+  console.log("try 1");
+  if (true) {
+    throw new Error("error!!!");
+  }
+  if (true) {
+    throw new Error("error 2!!!");
+  }
+  console.log("try 2");
+} catch (error) {
+  if (error instanceof TypeError) {
+    console.error("TypeError: ", error);
+  } else if (error instanceof SyntaxError) {
+    console.error("SyntaxError: ", error);
+  } else {
+    console.error(error);
+  }
+} finally {
+  console.log("finally");
 }
+console.log("end");
