a152c0a1f125f2bf7e5c7e56a5cb94bc4a6238d6
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-10 09:37:39 +0300

    Add work with Error

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,20 @@
 "use strict";
 
-function timeout(ms){
-  return new Promise((resolve, reject)=>{
-    setTimeout(()=>{resolve('hi from timeout')}, ms);
+fetch("./data.json")
+  .finally(() => {
+    console.log("start");
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    document.body.prepend(document.createTextNode('500'));
+    console.dir(error);
+    if (error instanceof TypeError) {
+      console.error("Ошибка соединения");
+    }
+  })
+  .finally(() => {
+    console.log("end");
   });
-}
-
-timeout(2000)
-  .then((info)=>{console.log(info)})
-  .catch((info)=>{console.error(info)})
-
-
-
