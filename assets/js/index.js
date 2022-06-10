d5ca98e6ad02c7616a437189dadee25f5b535883
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-10 11:19:11 +0300

    Example delegation event

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,23 @@
 "use strict";
 
-fetch("./data.json")
-  .finally(() => {
-    console.log("start");
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    document.body.prepend(document.createTextNode('500'));
-    console.dir(error);
-    if (error instanceof TypeError) {
-      console.error("Ошибка соединения");
-    }
-  })
-  .finally(() => {
-    console.log("end");
-  });
+// fetch("./data.json")
+//   .finally(() => {
+//     console.log("start");
+//   })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     document.body.prepend(document.createTextNode('500'));
+//     if (error instanceof TypeError) {
+//       console.error("Ошибка соединения: ", error);
+//     }else if (error instanceof SyntaxError) {
+//       console.error("Проверь запятые: ", error);
+//     }else{
+//       console.error(error);
+//     }
+//   })
+//   .finally(() => {
+//     console.log("end");
+//   });
--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,20 +1,2 @@
 "use strict";
 
-function handlePromise(promise) {
-  return promise
-    .then((data) => {
-      console.log("in func: ", data);
-    })
-    .catch((err) => {
-      console.error("in func: ", err);
-    });
-}
-
-const number = 45;
-
-// const numberPromise = new Promise((resolve, reject)=>{
-//   resolve(number);
-// })
-
-handlePromise(Promise.resolve(number));
-handlePromise(Promise.reject('error'));
