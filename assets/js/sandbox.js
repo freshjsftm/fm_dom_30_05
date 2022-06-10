14123a32833002621d9b685cef3d72712b2ae823
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-10 10:46:08 +0300

    Example mikro/makro

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,27 +1,31 @@
 "use strict";
 
-// try {} catch{}
-// try {} finally{}
-
-console.log("start");
-try {
-  console.log("try 1");
-  if (true) {
-    throw new Error("error!!!");
-  }
-  if (true) {
-    throw new Error("error 2!!!");
-  }
-  console.log("try 2");
-} catch (error) {
-  if (error instanceof TypeError) {
-    console.error("TypeError: ", error);
-  } else if (error instanceof SyntaxError) {
-    console.error("SyntaxError: ", error);
-  } else {
-    console.error(error);
-  }
-} finally {
-  console.log("finally");
-}
-console.log("end");
+console.log('start');   ///sin
+setTimeout(             ///sin
+  ()=>{
+  console.log('setTimeout');    //makro
+},0);
+const pr = new Promise((resolve, reject)=>{  ///sin
+  console.log('start promise');   ///sin
+  resolve();                      ///sin
+  reject();                       ///sin
+  console.log('end promise');     ///sin
+});
+pr                                ///sin
+  .then(                          ///sin
+    ()=>{                         //mikro
+    console.log('resolve start');        ///sin
+    setTimeout(()=>{
+      console.log('setTimeout 2');    //makro
+    },0);
+    console.log('resolve end'); 
+  })
+  .catch(                          ///sin
+    ()=>{                         //mikro
+    console.log('reject')
+  })
+  .finally(                          ///sin
+    ()=>{                         //mikro
+    console.log('finally')
+  })
+console.log('end');                   ///sin
